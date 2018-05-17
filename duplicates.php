a2fc116e81124295c220479ba84b38c4bad1b98e
Jorge Monteiro <jsoucasaux@gmail.com>
2018-05-17 19:47:39 +0100

    dups

--- duplicates.php
+++ duplicates.php
@@ -3,30 +3,26 @@
 		<div class="dups">
 			<div class="container-fluid">
 				<h1>Duplicate tracks on playlists</h1>
-				<div class="faketable">
-					<table class="table table-sm" id="pltracks">
-					<thead>
-						<tr class="name">bmtkn</tr>
-					</thead>
-					<tbody></tbody>
-				</table>
-				<table class="table table-sm" id="pltracks">
-					<thead>
-						<tr class="name">bmtkn</tr>
-					</thead>
-					<tbody></tbody>
-				</table>
-				<table class="table table-sm" id="pltracks">
-					<thead>
-						<tr class="name">bmtkn</tr>
-					</thead>
-					<tbody></tbody>
-				</table>
+				<h2>Total tracks: <span>0</span></h2>
+				<h2>Get suspicious or all playlists <div class="badge">AND</div> <a href="javascript:void(0);" id="checkdups">Check for duplicate songs</a> <div class="badge">OR</div> <a href="javascript:void(0);" id="checktrack">Check a track</a></h2>
+				
+				<div class="row justify-content-md-center d-none">
+					<div class="col-md-4">
+						<div class="input-group mb-3">
+							<input type="text" class="form-control" placeholder="Track ID or Spotify URI" id="trackval">
+							<div class="input-group-append">
+								<button class="btn btn-success" id="searchtrack" type="button">Search visible playlists</button>
+							</div>
+						</div>
+					</div>
 				</div>
-				<style>
-				.faketable { display: table; }
-				.faketable table { display: table-cell; }
-				</style>
+				
+				<h2 class="dupwarn"></h2>
+				
+				<table class="table table-sm" id="pltracks">
+					<thead><tr></tr></thead>
+					<tbody><tr></tr></tbody>
+				</table>
 			</div>
 		</div>
 	</div>
--- js/duplicates-backup-2.js
+++ js/duplicates-backup-2.js
@@ -21,7 +21,6 @@ var offset = 0;
 var tracksCells = "";
 
 var totalTracks = 0;
-var allTracks = [];
 
 // Get playlist's tracks
 function getPlaylistTracks(playlistId) {
@@ -38,10 +37,11 @@ function getPlaylistTracks(playlistId) {
 				for (var t = 0; t < tracks.items.length; t++) {
 					//console.log(tracks.items[t].track.artists[0].name + " - " + tracks.items[t].track.name);
 					//$('.dups tr[data-plid="' + playlistId + '"] .track').append("<span>" + tracks.items[t].track.name + "</span>");
+					
 					tracksCells +=
-					`<span data-track="${ tracks.items[t].track.id }">${ tracks.items[t].track.name }<span class="artist">${ tracks.items[t].track.artists[0].name }</span></span>`;
+					`<span data-track="${ tracks.items[t].track.artists[0].name }">${ tracks.items[t].track.artists[0].name } - ${ tracks.items[t].track.name }</span>`;
+					
 					totalTracks++;
-					allTracks.push( tracks.items[t].track.id );
 					$("h1 + h2 span").text(totalTracks);
 				}
 				getPlaylistTracks(playlistId);
@@ -49,13 +49,16 @@ function getPlaylistTracks(playlistId) {
 				offset = 0;
 				numPages = 0;
 				pageCounter = 0;
-				//console.log(allTracks);
+				//console.log(tracksCells); // outputed only once
 				$("#pltracks td.p-" + playlistId).html(tracksCells);
 				tracksCells = "";
 			}
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) { console.log('error'); }
 	});
+	
+	
+	
 }
 
 // Favourite playlists
@@ -65,69 +68,31 @@ function getThisPlaylist(favPlaylistID) {
 		async: true,
 		headers: { 'Authorization': 'Bearer ' + token },
 		success: function(thePlaylist) {
-			var plName =`
-			<th class="p-${ favPlaylistID }">
-				<a target="_blank" href="${ thePlaylist.external_urls.spotify }">${ thePlaylist.name }</a>
-			</th>`;
-			var plCells = `<td class="p-${ favPlaylistID }"><a data-id="${ favPlaylistID }" class="btn btn-primary btn-sm gettracks">GET</a></td>`;
+			var plName = `<th class="p-${ favPlaylistID }"><a target="_blank" href="${ thePlaylist.external_urls.spotify }">${ thePlaylist.name }</a></th>`;
+			var plCells = `<td class="p-${ favPlaylistID }"></td>`;
 			$("#pltracks thead tr").append(plName);
 			$("#pltracks tbody tr").append(plCells);
 		},
 		error: function() {console.log("ajax error getting playlists");},
-		/*complete: function () {
+		complete: function () {
 			for (var p = 0; p < favGenres.length; p++) {
 				getPlaylistTracks(favGenres[p]);
 			}
-		}*/
+		}
 	});
 }
 
-function getAllPlaylists() {
+function getAllPlaylists( callback ) {
 	for (var p = 0; p < favGenres.length; p++) {
 		getThisPlaylist(favGenres[p]);
 		//getPlaylistTracks(favGenres[p]);
 	}
 }
 
-$("#pltracks").on("click", ".gettracks", function(){
+/*$("#pltracks").on("click", ".gettracks", function(){
 	getPlaylistTracks( $(this).data("id") );
-	//console.log("clicked: " + $(this).data("id"));
-});
-
-$("#checkdups").click(function(){
-	var sorted_arr = allTracks.slice().sort();
-	var results = [];
-	for (var i = 0; i < sorted_arr.length - 1; i++) {
-		if (sorted_arr[i + 1] == sorted_arr[i]) {
-			results.push(sorted_arr[i]);
-		}
-	}
-	if ( results.length == 0 ) {
-		$("h2.dupwarn").text("There's " + results.length + " duplicates. Hurray!!");
-	} else {
-		$("h2.dupwarn").text("There's " + results.length + " duplicates");
-	}
-	
-	for (var d = 0; d < results.length; d++) {
-		$("[data-track='" + results[d] + "']").addClass("double");
-	}
-	console.log(results);
-});
-
-$("#checktrack").click(function() {
-	$(".justify-content-md-center").toggleClass("d-none");
-});
-
-$("#searchtrack").click(function() {
-	// test track at PT  ---  0zFELNYR05rYzmj4lSLoCX
-	var trackToSearch = $("#trackval").val();
-	trackToSearch = trackToSearch.split("spotify:track:").pop();
-	var contains = allTracks.includes(trackToSearch);
-	if (contains) {
-		$("h2.dupwarn").text("The track " + trackToSearch + " is present at one of this playists");
-		$("[data-track='" + trackToSearch + "']").addClass("double");
-	}
-});
+	console.log("clicked: " + $(this).data("id"));
+});*/
 
 
 $(document).ready(function() {
